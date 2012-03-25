a7dfda1bfc0e567052345db2ef7808c988ef1a08
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-25 13:37:42 -0430

    Estandarizando la conexión

--- conf/conexion.php
+++ conf/conexion.php
@@ -1,6 +1,8 @@
 <?
+$db_hostname = "localhost";
+$db_database = "db_cadeveher";
+$db_username = "user_cadeveher";
+$db_password = "123456";
 
-$conn = pg_connect("host=localhost port=5432 dbname=cadeveher password=12345 user=postgres")
-or die("<BR>ERROR accedint a la BD!<BR>"); 
-
+$db_connect = pg_connect("host=$db_hostname dbname=$db_database user=$db_username password=$db_password") or die ("Imposible conectarse al servidor " . pg_last_error());
 ?>
