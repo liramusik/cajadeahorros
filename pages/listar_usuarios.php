67e0a5d08110f4e3609429c6b743591cee7df2a1
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-25 15:46:25 -0430

    Vista para listar usuarios

---
+++ pages/listar_usuarios.php
@@ -0,0 +1,26 @@
+<?php
+$db_hostname = "localhost";
+$db_database = "db_cadeveher";
+$db_username = "user_cadeveher";
+$db_password = "123456";
+
+$db_connect = pg_connect("host=$db_hostname dbname=$db_database user=$db_username password=$db_password") or die ("Imposible conectarse al servidor " . pg_last_error());
+
+$query = "select * from tb_usuarios where estatus=true";
+
+$result = pg_query($db_connect, $query);
+if(!$result) {
+	print "Error" . pg_last_error();
+}
+?>
+
+<table>
+	<?php while($rows = pg_fetch_object($result)): ?>
+		<tr>
+			<td><?php print $rows->nombre; ?></td>
+			<td><?php print $rows->apellido; ?></td>
+			<td><a href="<?php print "index.php?page=modificar_usuarios&m_cedula=" . $rows->cedula; ?>">Modificar</a></td>
+			<td><a href="<?php print "index.php?page=listar_usuarios&d_cedula=" . $rows->cedula; ?>">Deshabilitar</a></td>
+		</tr>
+	<?php endwhile; ?>
+</table>
