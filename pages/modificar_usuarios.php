6ebce7927888cf16612150bbbbbe1aa5e06a76e6
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-25 18:35:58 -0430

    Asignand valores al formulario

--- pages/modificar_usuarios.php
+++ pages/modificar_usuarios.php
@@ -8,7 +8,7 @@ $db_connect = pg_connect("host=$db_hostname dbname=$db_database user=$db_usernam
 
 $cedula = $_GET['m_cedula'];
 
-$query_usuarios = "select * from tb_usuarios left join tb_nacionalidad on id_nacionalidad = id where cedula=$cedula";
+$query_usuarios = "select * from tb_usuarios left join tb_nacionalidad on id_nacionalidad = tb_nacionalidad.id left join tb_roles on id_rol = tb_roles.id where cedula=$cedula;";
 $query_roles = "select * from tb_roles";
 
 $result_usuarios = pg_query($db_connect, $query_usuarios);
@@ -32,6 +32,8 @@ while($rows = pg_fetch_object($result_usuarios)) {
 	$fecha_ingreso = date("d-m-Y", $rows->fecha_ingreso);
 	$fecha_egreso = !empty($rows->fecha_egreso) ? date("d-m-Y", $rows->fecha_egreso) : "";
 	$nacionalidad = $rows->nacionalidad;
+	$id_rol = $rows->id_rol;
+	$usuario = $rows->usuario;
 }
 ?>
 
@@ -46,7 +48,7 @@ while($rows = pg_fetch_object($result_usuarios)) {
 					<label for="tipe_user">Tipo de usuario: *</label>
 					<select name="select">
 						<?php while($rows = pg_fetch_object($result_roles)): ?>
-							<option value="<?php print $rows->id; ?>"><?php print $rows->descripcion; ?></option>
+							<option value="<?php print $rows->id; ?>" <?php ($id_rol == $rows->id) ? print "selected" : print ""; ?>><?php print $rows->descripcion; ?></option>
 						<?php endwhile; ?>
 					</select>
 				</td>
@@ -83,56 +85,38 @@ while($rows = pg_fetch_object($result_usuarios)) {
 				<td>
 			</tr>
 			<tr>
-			<td>
-				<label for="datein_user">Fecha de ingreso : *</label><br>
-				<input type="number" name="datein_user" id="datein_user" title="Fecha de ingreso" maxlength="40" placeholder="Fecha de ingreso" autocomplete="off" value="<?php print $fecha_ingreso; ?>" required />
-			</td>
-			<td>
-				<label for="datexit_user">Fecha de egreso : *</label><br>
-				<input type="number" name="datexit_user" id="datexit_user" title="Fecha de egreso" maxlength="40" placeholder="Fecho de egreso" autocomplete="off" value="<?php print $fecha_egreso; ?>" required />
-			</td>
-        </tr>
-        </table>
-    </fieldset>
-    <p>
-    <fieldset>
-        <legend>Información caja de ahorro</legend>
-        <table>
-        <tr>
-        	<td>        	
-        		<label for="initial_user">Aporte inicial: *</label><br>
-        		<input type="initial-user" name="initial_user" id="initial_user" title="Direccion Legal" maxlength="40" placeholder="Direccion legal" autocomplete="off" required/>
-                </td>
-        	<td>
-        		<label for="monthly_user">Aporte mensual : *</label><br>
-        		<input type="mothly_user" name="monthly_user" id="monthly_user" title="Aporte mensual" maxlength="40" placeholder="Aporte mensual" autocomplete="off" required/>
-        	</td>
-        </tr>
-        </table>
-    </fieldset>
-    <p>
-    <fieldset>
-        <legend>Información cuenta</legend>
-        <table>
-        <tr>
-        	<td>
-        		<label for="user_user">Usuario : *</label><br>
-        		<input type="text" name="user_user" id="user_user" title="Usuario" maxlength="40" placeholder="Usuario" autocomplete="off" required/>
-                </td>
-                </tr>
-        <tr>
-        	<td>
-        		<label for="password_user">Contrasena : *</label><br>
-        		<input type="password" name="password_user" id="password_user" title="Contrasena" maxlength="40" placeholder="Contrasena" autocomplete="off" required/>
-        	</td>
-        	<td>
-        		<label for="confir_user">Confirmar contrasena : *</label><br>
-        		<input type="password" name="confir_user" id="confir_user" title="Confirmar contrasena" maxlength="40" placeholder="Confirmar contrasena" autocomplete="off" required/>
-        	</td>
-        </tr>
-        </table>
-    </fieldset>
-    <p>
-   <input id="submit" type="submit" value="Registrar" name="submit" class="boton1"/>
-   <div id="message"></div>
+				<td>
+					<label for="datein_user">Fecha de ingreso : *</label><br>
+					<input type="number" name="datein_user" id="datein_user" title="Fecha de ingreso" maxlength="40" placeholder="Fecha de ingreso" autocomplete="off" value="<?php print $fecha_ingreso; ?>" required />
+				</td>
+				<td>
+					<label for="datexit_user">Fecha de egreso : *</label><br>
+					<input type="number" name="datexit_user" id="datexit_user" title="Fecha de egreso" maxlength="40" placeholder="Fecho de egreso" autocomplete="off" value="<?php print $fecha_egreso; ?>" required />
+				</td>
+			</tr>
+		</table>
+	</fieldset>
+	<fieldset class="informacion-cuenta" style="margin: 10px 0;">
+		<legend>Información cuenta</legend>
+		<table>
+			<tr>
+				<td>
+					<label for="user_user">Usuario : *</label><br>
+					<input type="text" name="user_user" id="user_user" title="Usuario" maxlength="40" placeholder="Usuario" autocomplete="off" value="<?php print $usuario; ?>" disabled required/>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<label for="password_user">Contrasena : *</label><br>
+					<input type="password" name="password_user" id="password_user" title="Contrasena" maxlength="40" placeholder="Contrasena" autocomplete="off" required/>
+				</td>
+				<td>
+					<label for="confir_user">Confirmar contrasena : *</label><br>
+					<input type="password" name="confir_user" id="confir_user" title="Confirmar contrasena" maxlength="40" placeholder="Confirmar contrasena" autocomplete="off" required/>
+				</td>
+			</tr>
+		</table>
+	</fieldset>
+	<input id="submit" type="submit" value="Registrar" name="submit" class="boton1"/>
+	<div id="message"></div>
 </form>
