975b87a3a7a0f8e87b2c255303061c9bb0b8018f
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-26 01:30:59 -0430

    Eliminando basura

--- includes/pages.php
+++ includes/pages.php
@@ -1,20 +1,15 @@
 <?php
 if (!isset($_SESSION['session_usuario'])){
-
 		if (!isset($_GET['page'])) {
     			include("pages/acceso.php");
 		} else {
     			include("pages/".$_GET['page'].".php");
 		}
-
-	}else{
-
+	} else {
 		if (!isset($_GET['page'])) {
                         include("pages/bienvenida.php");
                 } else {
                         include("pages/".$_GET['page'].".php");
                 }
-
-
 }
 ?>
