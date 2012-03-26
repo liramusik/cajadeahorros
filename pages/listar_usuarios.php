616b8761b0faaed10dd54ff2423392133f6e1eb2
David Mora <gochirila000@yahoo.es>
2012-03-26 00:07:13 -0430

    Permisos

--- pages/listar_usuarios.php
+++ pages/listar_usuarios.php
@@ -1,29 +1,33 @@
 <?php if(isset($_SESSION['session_usuario'])): ?>
+	<?php if($_SESSION['session_id_rol'] == 1): ?>
 
-<?php
-	$db_hostname = "localhost";
-	$db_database = "db_cadeveher";
-	$db_username = "user_cadeveher";
-	$db_password = "123456";
 
-	$db_connect = pg_connect("host=$db_hostname dbname=$db_database user=$db_username password=$db_password") or die ("Imposible conectarse al servidor " . pg_last_error());
+	<?php
+		
+		$db_hostname = "localhost";
+		$db_database = "db_cadeveher";
+		$db_username = "user_cadeveher";
+		$db_password = "123456";
 
-	$query = "select * from tb_usuarios where estatus=true";
+		$db_connect = pg_connect("host=$db_hostname dbname=$db_database user=$db_username password=$db_password") or die ("Imposible conectarse al servidor " . pg_last_error());
 
-	$result = pg_query($db_connect, $query);
-	if(!$result) {
-		print "Error" . pg_last_error();
-	}
-	?>
+		$query = "select * from tb_usuarios where estatus=true";
 
-	<table>
-		<?php while($rows = pg_fetch_object($result)): ?>
-			<tr>
-				<td><?php print $rows->nombre; ?></td>
-				<td><?php print $rows->apellido; ?></td>
-				<td><a href="<?php print "index.php?page=modificar_usuarios&m_cedula=" . $rows->cedula; ?>">Modificar</a></td>
-				<td><a href="<?php print "index.php?page=listar_usuarios&d_cedula=" . $rows->cedula; ?>">Deshabilitar</a></td>
-			</tr>
-		<?php endwhile; ?>
-	</table>
+		$result = pg_query($db_connect, $query);
+		if(!$result) {
+			print "Error" . pg_last_error();
+		}
+		?>
+
+		<table>
+			<?php while($rows = pg_fetch_object($result)): ?>
+				<tr>
+					<td><?php print $rows->nombre; ?></td>
+					<td><?php print $rows->apellido; ?></td>
+					<td><a href="<?php print "index.php?page=modificar_usuarios&m_cedula=" . $rows->cedula; ?>">Modificar</a></td>
+					<td><a href="<?php print "index.php?page=listar_usuarios&d_cedula=" . $rows->cedula; ?>">Deshabilitar</a></td>
+				</tr>
+			<?php endwhile; ?>
+		</table>
+	<?php endif; ?>
 <?php endif; ?>
