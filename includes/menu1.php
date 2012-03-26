fa19a92d11064bcbe9db879c72f32246f48db9c9
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-26 02:21:12 -0430

    Agregando el menú del administrador

---
+++ includes/menu1.php
@@ -0,0 +1,14 @@
+<div id="menu-principal">
+	<ul>
+		<li>
+			<a href="#">Usuarios</a>
+			<ul>
+				<li><a href="index.php?page=listar_usuarios">Ejemplo1</a></li>
+				<li><a href="index.php?page=listar_usuarios">Ejemplo2</a></li>
+				<li><a href="index.php?page=listar_usuarios">Ejemplo3</a></li>
+				<li><a href="index.php?page=listar_usuarios">Ejemplo4</a></li>
+			</ul>
+
+		</li>
+	</ul>
+</div>
