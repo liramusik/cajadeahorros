a84a5e7d3f5dc538a8cac574bb17d2e98da68f46
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-26 02:19:50 -0430

    Configurando los acceso al menu según el rol

--- includes/menu.php
+++ includes/menu.php
@@ -1,10 +1,15 @@
-<div id="menu">
-    	<a href="index.php">Principal</a>&nbsp;&nbsp;|&nbsp;&nbsp;    
-    <a href="index.php?page=pantalla_usuario">usuario</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-    <a href="index.php?page=pantalla_instalacion1">Instalacion1</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-    <a href="index.php?page=pantalla_instalacion2">Instalacion2</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-    <a href="index.php?page=pantalla_configuracion">Configuracion</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-    <a href="index.php?page=pantalla_configuracion1">Configuracion1</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-    <a href="index.php?page=pantalla_banco">Banco</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-    <a href="index.php?page=acceso">Acceso</a>
-</div>
+<?php
+if(isset($_SESSION['session_usuario'])) {
+	switch($_SESSION['session_id_rol']) {
+		case 1:
+			include("includes/menu1.php");
+			break;
+		case 2:
+			include("includes/menu2.php");
+			break;
+		case 3:
+			include("includes/menu2.php");
+			break;
+	}
+}
+?>
