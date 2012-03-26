fb326eac2ebc2ba6e48ca7c5baba86c33ee790b7
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-26 14:24:35 -0430

    Cambiando el menu del administrador

--- includes/menu1.php
+++ includes/menu1.php
@@ -4,14 +4,7 @@
 			<a href="index.php">Inicio</a>
 		</li>
 		<li>
-			<a href="#">Usuarios</a>
-			<ul>
-				<li><a href="index.php?page=listar_usuarios">Ejemplo1</a></li>
-				<li><a href="index.php?page=listar_usuarios">Ejemplo2</a></li>
-				<li><a href="index.php?page=listar_usuarios">Ejemplo3</a></li>
-				<li><a href="index.php?page=listar_usuarios">Ejemplo4</a></li>
-			</ul>
-
+			<li><a href="index.php?page=listar_usuarios">Usuarios</a></li>
 		</li>
 		<li>
 			<a href="index.php?page=simulacion">Simulación</a>
