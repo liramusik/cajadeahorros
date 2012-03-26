b6527446c4b58ac2dfddccde4fea7398e0f06f63
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-26 03:05:16 -0430

    Validando la sesión

--- pages/listar_usuarios.php
+++ pages/listar_usuarios.php
@@ -1,5 +1,5 @@
 <?php if(isset($_SESSION['session_usuario'])): ?>
-	<?php if($_SESSION['session_id_rol'] == 2): ?>
+	<?php if($_SESSION['session_id_rol'] == 1): ?>
 		<?php
 		$db_hostname = "localhost";
 		$db_database = "db_cadeveher";
