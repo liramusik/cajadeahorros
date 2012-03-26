20992779608a6547d1c70f449050960e804987f7
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-26 17:59:07 -0430

    Agrupando valores en el menú

--- includes/menu1.php
+++ includes/menu1.php
@@ -7,10 +7,15 @@
 			<li><a href="index.php?page=listar_usuarios">Usuarios</a></li>
 		</li>
 		<li>
-			<a href="index.php?page=solicitud-prestamo">Solicitud de Préstamo</a>
-		</li>
-		<li>
-			<a href="index.php?page=simulacion">Simulación</a>
+			<a href="#">Préstamo</a>
+			<ul>
+				<li>
+					<a href="index.php?page=solicitud-prestamo">Solicitud de Préstamo</a>
+				</li>
+				<li>
+					<a href="index.php?page=simulacion">Simulación</a>
+				</li>
+			</ul>
 		</li>
 	</ul>
 </div>
