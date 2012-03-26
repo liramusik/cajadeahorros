0255d064f021ecb1881b5394d9b315bd32883dfe
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-26 03:03:29 -0430

    Agregando opciones para el menu2

---
+++ includes/menu2.php
@@ -0,0 +1,8 @@
+<?php $cedula = $_SESSION['session_cedula']; ?>
+<div id="menu-principal">
+	<ul>
+		<li>
+			<a href="index.php?page=modificar_usuarios&m_cedula=<?php print $cedula; ?>">Cuenta</a>
+		</li>
+	</ul>
+</div>
