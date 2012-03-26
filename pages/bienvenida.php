8c421ebdbf6bcbb5304f4b0b1251d007cf4b703d
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-26 03:16:17 -0430

    Arreglando la bienvenida

--- pages/bienvenida.php
+++ pages/bienvenida.php
@@ -1,1 +1,2 @@
-</h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>
+<h1>Bienvenido</h1>
+<div>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
