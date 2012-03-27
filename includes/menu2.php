4dfaf9a51dd57d77817292ccffea3889cf36caf3
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-27 13:42:48 -0430

    Arreglando el enlace a cuenta y añadiendo la notificación

--- includes/menu2.php
+++ includes/menu2.php
@@ -5,7 +5,10 @@
 			<a href="index.php">Inicio</a>
 		</li>
 		<li>
-			<a href="index.php?page=modificar_usuarios&m_cedula=<?php print $cedula; ?>">Cuenta</a>
+			<a href="index.php?page=modificar-usuarios&m_cedula=<?php print $cedula; ?>">Cuenta</a>
+		</li>
+		<li>
+			<li><a href="index.php?page=listar-notificaciones">Notificaciones</a></li>
 		</li>
 		<li>
 			<a href="#">Préstamo</a>
