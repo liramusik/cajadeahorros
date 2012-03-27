917f6112a7b7841071422857b560271bc6d2cf31
David Mora <gochirila000@yahoo.es>
2012-03-26 21:23:12 -0430

    query insercion solicitud prestamo

--- pages/generar-solicitud.php
+++ pages/generar-solicitud.php
@@ -5,4 +5,5 @@ $tiempo = $_POST['tiempo'];
 $pago = $_POST['pago'];
 $observacion = $_POST['observacion'];
 $fecha_actual = time();
+$query = "INSERT INTO tb_solicitud_prestamo (cedula_usuario, monto, fecha, tiempo, observacion) VALUES ('$cedula','$monto','$fecha_actual','$tiempo','$observacion')";
 ?>
