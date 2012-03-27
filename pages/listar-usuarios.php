ddc9b8db57ba50902ff5fd257d69b283979fc9ce
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-27 13:43:53 -0430

    Agregando el enlace de notificaciones

--- pages/listar-usuarios.php
+++ pages/listar-usuarios.php
@@ -51,7 +51,7 @@
 					<td><?php print $rows->email; ?></td>
 					<td><a href="<?php print "index.php?page=notificar&n_cedula=" . $rows->cedula; ?>"><img src="img/notificar.png" title="Notificar" alt="Notificar"></a></td>
 					<td><a href="<?php print "index.php?page=modificar-usuarios&m_cedula=" . $rows->cedula; ?>"><img src="img/modificar.png" title="Modificar" alt="Modificar"></a></td>
-					<td><a href="<?php print "index.php?page=listar-usuarios&d_cedula=" . $rows->cedula; ?>"><img src="img/deshabilitar.png" title="Deshabilitar" alt="Deshabilitar"></a></td>
+					<td><a href="<?php print "index.php?page=deshabilitar-usuarios&d_cedula=" . $rows->cedula; ?>"><img src="img/deshabilitar.png" title="Deshabilitar" alt="Deshabilitar"></a></td>
 				</tr>
 			<?php endwhile; ?>
 		</table>
