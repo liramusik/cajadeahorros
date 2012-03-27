dd500e9275a1e63bc3035b4a20daaea5d0961075
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-26 22:12:17 -0430

    Arreglando el menú del usuario Asociado

--- includes/menu2.php
+++ includes/menu2.php
@@ -8,7 +8,15 @@
 			<a href="index.php?page=modificar_usuarios&m_cedula=<?php print $cedula; ?>">Cuenta</a>
 		</li>
 		<li>
-			<a href="index.php?page=simulacion">Simulación</a>
+			<a href="#">Préstamo</a>
+			<ul>
+				<li>
+					<a href="index.php?page=solicitud-prestamo">Solicitud de Préstamo</a>
+				</li>
+				<li>
+					<a href="index.php?page=simulacion">Simulación</a>
+				</li>
+			</ul>
 		</li>
 	</ul>
 </div>
