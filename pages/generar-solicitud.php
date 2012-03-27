dee66e9a505ba5a37511b83d00c00a37c373fea3
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-26 21:08:23 -0430

    Archivo que inserta la solicitud de préstamo

---
+++ pages/generar-solicitud.php
@@ -0,0 +1,8 @@
+<?php
+$cedula = $_SESSION['session_cedula'];
+$monto = $_POST['monto'];
+$tiempo = $_POST['tiempo'];
+$pago = $_POST['pago'];
+$observacion = $_POST['observacion'];
+$fecha_actual = time();
+?>
