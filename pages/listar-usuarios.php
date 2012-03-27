c3cb08338665c95d85941384f203cc08177ba40a
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-27 17:00:09 -0430

    Haciendo pruebas

--- pages/listar-usuarios.php
+++ pages/listar-usuarios.php
@@ -1,18 +1,17 @@
 <?php if(isset($_SESSION['session_usuario'])): ?>
 	<?php if($_SESSION['session_id_rol'] == 1): ?>
-		<?php
+	<?php
 		$db_hostname = "localhost";
 		$db_database = "db_cadeveher";
 		$db_username = "user_cadeveher";
 		$db_password = "123456";
-
 		$db_connect = pg_connect("host=$db_hostname dbname=$db_database user=$db_username password=$db_password") or die ("Imposible conectarse al servidor " . pg_last_error());
 
 		$query = "select * from tb_usuarios where estatus=true";
 
 		$result = pg_query($db_connect, $query);
 		if(!$result) {
-			print "Error" . pg_last_error();
+			print "Error " . pg_last_error();
 		}
 		?>
 		<h1>Listado de Usuarios</h1>
