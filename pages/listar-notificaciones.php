41629639f9492c75be5477baf9b04785194917eb
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-27 13:51:02 -0430

    Arreglando el listado

--- pages/listar-notificaciones.php
+++ pages/listar-notificaciones.php
@@ -1,68 +1,60 @@
 <?php if(isset($_SESSION['session_usuario'])): ?>
-		<?php
-		$db_hostname = "localhost";
-		$db_database = "db_cadeveher";
-		$db_username = "user_cadeveher";
-		$db_password = "123456";
+	<?php
+	$db_hostname = "localhost";
+	$db_database = "db_cadeveher";
+	$db_username = "user_cadeveher";
+	$db_password = "123456";
 
-		$cedula = $_SESSION['session_cedula'];
+	$cedula = $_SESSION['session_cedula'];
 
-		$db_connect = pg_connect("host=$db_hostname dbname=$db_database user=$db_username password=$db_password") or die ("Imposible conectarse al servidor " . pg_last_error());
+	$db_connect = pg_connect("host=$db_hostname dbname=$db_database user=$db_username password=$db_password") or die ("Imposible conectarse al servidor " . pg_last_error());
 
-<<<<<<< HEAD
+	if($_SESSION['session_id_rol'] == 1) {
+		$query = "select id, fecha, asunto, nombre, apellido, email from tb_notificaciones left join tb_usuarios on cedula_usuario = cedula order by fecha desc";
+	} else {
+		$query = "select id, fecha, asunto, nombre, apellido, email from tb_notificaciones left join tb_usuarios on cedula_usuario = cedula where cedula=$cedula order by fecha desc";
+	}
 
-		if($_SESSION['session_id_rol'] == 1) {
-			$query = "select id, fecha, asunto, nombre, apellido, email from tb_notificaciones left join tb_usuarios on cedula_usuario = cedula order by fecha desc";
-		} else {
-			$query = "select id, fecha, asunto, nombre, apellido, email from tb_notificaciones left join tb_usuarios on cedula_usuario = cedula where cedula=$cedula order by fecha desc";
-		}
-=======
-		$query = "select id, fecha, asunto, nombre, apellido, email from tb_notificaciones left join tb_usuarios on cedula_usuario = cedula order by fecha desc;";
->>>>>>> 0e4dd75a2bf08e7abaa6e1733a43b2c9e753c746
-
-		$result = pg_query($db_connect, $query);
-		if(!$result) {
-			print "Error" . pg_last_error();
-		}
-		?>
-		<h1>Listado de Usuarios</h1>
-		<script>
-			$(document).ready(function() {
-				$('#listado').dataTable({
-					"bLengthChange": false,
-					"bInfo": false,
-					"oLanguage": {
-						"sSearch": "",
-						"oPaginate": {
-							"sPrevious": "Atras",
-							"sNext": "Siguiente"
-						}
+	$result = pg_query($db_connect, $query);
+	if(!$result) {
+		print "Error" . pg_last_error();
+	}
+	?>
+	<h1>Listado de Usuarios</h1>
+	<script>
+		$(document).ready(function() {
+			$('#listado').dataTable({
+				"bLengthChange": false,
+				"bInfo": false,
+				"oLanguage": {
+					"sSearch": "",
+					"oPaginate": {
+						"sPrevious": "Atras",
+						"sNext": "Siguiente"
 					}
-				});
+				}
 			});
-		</script>
-		<table id="listado">
-			<thead>
-				<tr>
-					<th>Fecha</th>
-					<th>Nombre</th>
-					<th>Email</th>
-					<th>Asunto</th>
-				</tr>
-			</thead>
-			<?php while($rows = pg_fetch_object($result)): ?>
-				<tr>
-					<td><?php print date("d-m-Y H:i", $rows->fecha); ?></td>
-					<td><?php print $rows->nombre . " " . $rows->apellido; ?></td>
-					<td><?php print $rows->email; ?></td>
-<<<<<<< HEAD
-					<td><a href="index.php?page=mostrar-notificacion&n_notificacion=<?php print $rows->id; ?>"><?php print $rows->asunto; ?></a></td>
-=======
-					<td><a href="index.php?page=descripcion-notificacion&n_notificacion=<?php print $rows->id; ?>"><?php print $rows->asunto; ?></a></td>
->>>>>>> 0e4dd75a2bf08e7abaa6e1733a43b2c9e753c746
-				</tr>
-			<?php endwhile; ?>
-		</table>
-       <?php else: ?>
-                <div class="mensaje">Usted no posee privilegios ver esta pagina <a href="index.php">Regresar</a></div>
+		});
+	</script>
+	<table id="listado">
+		<thead>
+			<tr>
+				<th>Fecha</th>
+				<th>Nombre</th>
+				<th>Email</th>
+				<th>Asunto</th>
+			</tr>
+		</thead>
+		<?php while($rows = pg_fetch_object($result)): ?>
+			<tr>
+				<td><?php print date("d-m-Y H:i", $rows->fecha); ?></td>
+				<td><?php print $rows->nombre . " " . $rows->apellido; ?></td>
+				<td><?php print $rows->email; ?></td>
+				<td><a href="index.php?page=mostrar-notificacion&n_notificacion=<?php print $rows->id; ?>"><?php print $rows->asunto; ?></a></td>
+				<td><a href="index.php?page=descripcion-notificacion&n_notificacion=<?php print $rows->id; ?>"><?php print $rows->asunto; ?></a></td>
+			</tr>
+		<?php endwhile; ?>
+	</table>
+<?php else: ?>
+	<div class="mensaje">Usted no posee privilegios ver esta pagina <a href="index.php">Regresar</a></div>
 <?php endif; ?>
