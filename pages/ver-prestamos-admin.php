ba29d2c5b15c4a4537403b9e7232a40780e706ef
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-03-30 22:52:23 -0430

    Módulo de préstamos

--- pages/ver-prestamos-admin.php
+++ pages/ver-prestamos-admin.php
@@ -1,2 +1,19 @@
 <?php
+$solicitud = $_GET['solicitud'];
+$x = new connection();
+
+$x->setQuery("select tb_solicitud_prestamo.id, cedula, nombre, apellido, monto, fecha, tiempo, porcentaje, observacion, respuesta, tb_estatus_solicitud_prestamo.estatus from tb_solicitud_prestamo left join tb_estatus_solicitud_prestamo on tb_solicitud_prestamo.id_estatus_solicitud_prestamo = tb_estatus_solicitud_prestamo.id left join tb_usuarios on cedula_usuario = cedula where tb_solicitud_prestamo.id=$solicitud");
+while($rows = pg_fetch_object($x->getQuery())) {
+	$cedula = $rows->cedula;
+	$nombre = $rows->nombre;
+	$apellido = $rows->apellido;
+	$monto = $rows->monto;
+	$fecha = $rows->fecha;
+	$tiempo = $rows->tiempo;
+	$porcentaje = $rows->porcentaje;
+	$observacion = $rows->observacion;
+	$respuesta = $rows->respuesta;
+	$estatus = $rows->estatus;
+}
+unset($x);
 ?>
--- pages/ver-prestamos.php
+++ pages/ver-prestamos.php
@@ -1,15 +1,13 @@
 <?php if(isset($_SESSION['session_usuario'])): ?>
+	<h1>Detalles de Préstamo</h1>
 	<?php
 	$solicitud = $_GET['solicitud'];
 	if($_SESSION['session_id_rol'] == 1) {
-		$c->setQuery("select tb_solicitud_prestamo.id, cedula, nombre, apellido, monto, fecha, tb_estatus_solicitud_prestamo.estatus from tb_solicitud_prestamo left join tb_estatus_solicitud_prestamo on tb_solicitud_prestamo.id_estatus_solicitud_prestamo = tb_estatus_solicitud_prestamo.id left join tb_usuarios on cedula_usuario = cedula where tb_solicitud_prestamo.id=$solicitud");
 		include("ver-prestamos-admin.php");
 	} else {
-		$cedula = $_SESSION['session_cedula'];
+		include("ver-prestamos-usuarios.php");
 	}
 	?>
-	<h1>Detalles de Préstamo</h1>
-	<h2></h2>
 <?php else: ?>
 	<div class="mensaje">Usted no posee privilegios ver esta pagina <a href="index.php">Regresar</a></div>
 <?php endif; ?>
