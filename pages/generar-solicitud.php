d17538f7a22f09ef2cdb19ed4422e84073c38057
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-01 00:44:28 -0430

    Arreglando espacios inecesarios

--- pages/generar-solicitud.php
+++ pages/generar-solicitud.php
@@ -6,6 +6,5 @@ $pago = $_POST['pago'];
 $observacion = trim($_POST['observacion']);
 $fecha = date("Y-m-d", time());
 $c->setQuery("insert into tb_solicitud_prestamo(id_tipo_pago, cedula_usuario, monto, fecha, tiempo, observacion) values($pago, $cedula, $monto, '" . $fecha . "', $tiempo, '" . $observacion . "')");
-
 ?>
 <div class="mensaje">Su solicitud de prestamos se realizo con exito <a href="index.php">Aceptar</a></div>
