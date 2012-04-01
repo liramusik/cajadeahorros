a7730c466363d8ffaac25aad412cf62d1e35d168
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-01 14:33:05 -0430

    Validando el acceso

--- pages/acceso.php
+++ pages/acceso.php
@@ -6,14 +6,14 @@
 		<table>
 			<tr>
 				<td>
-					<label for="user_login">Usuario :</label><br>
-					<input type="text" name="user_login" id="user_login" title="Usuario" maxlength="60" placeholder="Usuario" autocomplete="off" required/>
+					<label for="usuario">Usuario <span class="obligatorio">*</span></label><br>
+					<input type="text" name="usuario" id="usuario" title="Usuario" maxlength="60" placeholder="Usuario" autocomplete="off" required />
 				</td>
 			</tr>
 			<tr>
 				<td>
-					<label for="password_login">Contrasena :</label><br>
-					<input type="password" name="password_login" id="password_login" title="contrasena" maxlength="40" placeholder="Password" autocomplete="off" required/>
+					<label for="password">Contraseña <span class="obligatorio">*</span></label><br>
+					<input type="password" name="password" id="password" title="Contraseña" maxlength="40" placeholder="Contraseña" autocomplete="off" required />
 				</td>	
 			</tr>
 		</table>
