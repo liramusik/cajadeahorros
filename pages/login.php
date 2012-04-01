d21fdc12b3f831ee930409575511ecd5d39c12dd
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-01 14:33:58 -0430

    Arreglando las variables a español

--- pages/login.php
+++ pages/login.php
@@ -1,9 +1,9 @@
 <?php
 include("login_class.php");
 
-$usuario = $_POST['user_login'];
-$password = md5($_POST['password_login']);
+$usuario = $_POST['usuario'];
+$password = md5($_POST['password']);
 
-$login=new login();
+$login = new login();
 $login->inicia(3600, $usuario, $password);
 ?>
