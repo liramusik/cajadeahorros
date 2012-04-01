06931c022b99c4e35be67bf6d67353f8510d1344
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-01 18:11:12 -0430

    Añadiendo la validación al formulario agregar-usuario

--- js/formularios.js
+++ js/formularios.js
@@ -5,14 +5,62 @@ $(document).ready(function(){
 			'password': 'required'
 		},
 		messages: {
-			'usuario': 'Es necesario ingresar un Usuario',
-			'password': 'Es necesario ingresar una Contraseña'
+			'usuario': 'Ingrese un usuario',
+			'password': 'Ingrese la contraseña'
 		}
 	});
 });
 $(document).ready(function(){
 	$("#agregar-usuario").validate({
 		rules: {
+			'nombre': 'required',
+			'nombre': {
+				required: true,
+				minlength: 3
+			},
+			'apellido': 'required',
+			'apellido': {
+				required: true,
+				minlength: 5
+			},
+			'cedula': 'required',
+			'cedula': {
+				required: true,
+				minlength: 8,
+				number: true
+			},
+			'telefono': 'required',
+			'telefono': {
+				required: true,
+				minlength: 11,
+				number: true
+			},
+			'email': 'required',
+			'email': {
+				required: true,
+				minlength: 15,
+				email: true
+			},
+			'direccion': 'required',
+			'direccion': {
+				required: true,
+				minlength: 15,
+			},
+			'fecha_ingreso': 'required',
+			'usuario': 'usuario',
+			'usuario': {
+				required: true,
+				minlength: 8,
+			},
+			password: {
+				required: true,
+				minlength: 5
+			},
+			confirmar_password: {
+				required: true,
+				minlength: 5,
+				equalTo: "#password"
+			}
 		},
 		messages: {
 		}
