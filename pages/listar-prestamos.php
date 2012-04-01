b3b40b0412cea84bcba44c532e6869ace3f3936c
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-01 02:11:20 -0430

    Arreglando el formato de la fecha

--- pages/listar-prestamos.php
+++ pages/listar-prestamos.php
@@ -1,10 +1,10 @@
 <?php if(isset($_SESSION['session_usuario'])): ?>
 	<?php
 	if($_SESSION['session_id_rol'] == 1) {
-		$c->setQuery("select tb_solicitud_prestamo.id, cedula, nombre, apellido, monto, fecha, tb_estatus_solicitud_prestamo.estatus from tb_solicitud_prestamo left join tb_estatus_solicitud_prestamo on tb_solicitud_prestamo.id_estatus_solicitud_prestamo = tb_estatus_solicitud_prestamo.id left join tb_usuarios on cedula_usuario = cedula order by fecha desc;");
+		$c->setQuery("select tb_solicitud_prestamo.id, cedula, nombre, apellido, monto, to_char(fecha, 'DD/MM/YYYY') as fecha, tb_estatus_solicitud_prestamo.estatus from tb_solicitud_prestamo left join tb_estatus_solicitud_prestamo on tb_solicitud_prestamo.id_estatus_solicitud_prestamo = tb_estatus_solicitud_prestamo.id left join tb_usuarios on cedula_usuario = cedula order by fecha desc;");
 	} else {
 		$cedula = $_SESSION['session_cedula'];
-		$c->setQuery("select tb_solicitud_prestamo.id, cedula, nombre, apellido, monto, fecha, tb_estatus_solicitud_prestamo.estatus from tb_solicitud_prestamo left join tb_estatus_solicitud_prestamo on tb_solicitud_prestamo.id_estatus_solicitud_prestamo = tb_estatus_solicitud_prestamo.id left join tb_usuarios on cedula_usuario = cedula where cedula=$cedula");
+		$c->setQuery("select tb_solicitud_prestamo.id, cedula, nombre, apellido, monto, to_char(fecha, 'DD/MM/YYYY') as fecha, tb_estatus_solicitud_prestamo.estatus from tb_solicitud_prestamo left join tb_estatus_solicitud_prestamo on tb_solicitud_prestamo.id_estatus_solicitud_prestamo = tb_estatus_solicitud_prestamo.id left join tb_usuarios on cedula_usuario = cedula where cedula=$cedula");
 	}
 	?>
 	<h1>Listado General de Préstamos</h1>
@@ -39,7 +39,7 @@
 				<td><a href="index.php?page=ver-prestamos&solicitud=<?php print $rows->id; ?>"><?php printf("No.- %05d", $rows->id); ?></a></td>
 				<td><?php print $rows->nombre . " " . $rows->apellido; ?></td>
 				<td><?php printf("%.2f", $rows->monto); ?></td>
-				<td><?php print date("d-m-Y H:i", $rows->fecha); ?></td>
+				<td><?php print $rows->fecha; ?></td>
 				<td><?php print $rows->estatus; ?></td>
 			</tr>
 		<?php endwhile; ?>
