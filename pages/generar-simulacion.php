1aa744820b2aefb2b96af328c16684c33ac42408
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-01 01:30:29 -0430

    Añadiendo el porcentaje de acuerdo al rol y a la fecha

--- pages/generar-simulacion.php
+++ pages/generar-simulacion.php
@@ -5,18 +5,28 @@ $tiempo = $_POST['tiempo'];
 $pago = $_POST['pago'];
 $fecha_actual = time();
 
+$rol = $_SESSION['session_id_rol'];
+$x = new connection();
+$x->setQuery("select porcentaje from tb_porcentajes where id_roles=$rol order by fecha desc limit 1;");
+while($rows = pg_fetch_object($x->getQuery())) {
+	$porcentaje = $rows->porcentaje;
+}
+unset($x);
+
 class simulacion {
 	private $cedula;
 	private $monto;
 	private $tiempo;
 	private $pago;
 	private $fecha;
-	public function __construct($p_cedula, $p_monto, $p_tiempo, $p_pago, $p_fecha) {
+	private $porcentaje;
+	public function __construct($p_cedula, $p_monto, $p_tiempo, $p_pago, $p_porcentaje, $p_fecha) {
 		$this->cedula = $p_cedula;
 		$this->monto = $p_monto;
 		$this->tiempo = $p_tiempo;
 		$this->pago = $p_pago;
 		$this->fecha = $p_fecha;
+		$this->porcentaje = $p_porcentaje;
 	}
 	public function generar() {
 		switch($this->pago) {
@@ -34,7 +44,7 @@ class simulacion {
 		$fechas = array();
 		$fechas[0] = $this->fecha;
 		for($i = 1; $i <= $this->tiempo; $i++) {
-			$intereses[$i] = (($this->monto * 6) / 100);
+			$intereses[$i] = (($this->monto * $this->porcentaje) / 100);
 			$total_intereses += $intereses[$i];
 			$fechas[$i] = strtotime("+1 month", $fechas[$i-1]);
 		}
@@ -50,7 +60,7 @@ class simulacion {
 		$monto[0] = $this->monto;
 		$cuota = ($monto[0] / $this->tiempo);
 		for($i = 1; $i <= $this->tiempo; $i++) {
-			$intereses[$i] = (($monto[$i - 1] * 6) / 100);
+			$intereses[$i] = (($monto[$i - 1] * $this->porcentaje) / 100);
 			$total_intereses += $intereses[$i];
 			$amortizacion[$i] = $intereses[$i] + $cuota;
 			$monto[$i] = $monto[$i-1] - $cuota;
@@ -63,7 +73,7 @@ class simulacion {
 	}
 }
 
-$sim = new simulacion($cedula, $monto, $tiempo, $pago, $fecha_actual);
+$sim = new simulacion($cedula, $monto, $tiempo, $pago, $porcentaje, $fecha_actual);
 $sim->generar();
 
 ?>
