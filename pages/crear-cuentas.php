c0ff4c4df49ab4010d7ff3e37f3444ec45215b87
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-04 17:04:33 -0430

    Solucionado el problema de crear nuevas cuentas

--- pages/crear-cuentas.php
+++ pages/crear-cuentas.php
@@ -23,18 +23,29 @@
 			unset($insertar_banco);
 		}
 
-		$buscar_cuenta = new connection();
-		$buscar_cuenta->setQuery("select id from tb_bancos where nombre='" . $nombre_banco . "'");
-		while($rows = pg_fetch_object($buscar_cuenta->getQuery())) {
+		$buscar_banco  = new connection();
+		$buscar_banco->setQuery("select id from tb_bancos where nombre='" . $nombre_banco . "'");
+		while($rows = pg_fetch_object($buscar_banco->getQuery())) {
 			$id_banco = $rows->id;
 		}
 
-		$insertar_cuenta = new connection();
-		$insertar_cuenta->setQuery("insert into tb_cuentas values(default, $id_banco, $tipo_cuenta, '" . $cuenta . "', default)");
-		if(!$insertar_cuenta->getQuery()) {
-			print '<div class="mensaje">Se ha generado un error <a href="index.php">Regresar</a></div>' . pg_last_error();
+		$buscar_cuenta = new connection();
+		$buscar_cuenta->setQuery("select id, cuenta from tb_cuentas where cuenta='" . $cuenta . "' limit 1;");
+		if(pg_num_rows($buscar_cuenta->getQuery()) > 0) {
+			while($rows = pg_fetch_object($buscar_cuenta->getQuery())) {
+				if($rows->cuenta == $cuenta) {
+					print '<div class="mensaje">Esta cuenta ya ha sido creada <a href="index.php">Regresar</a></div>';
+					print '<div class="mensaje">Si desea modificarla, haga clic <a href="index.php?page=modificar-cuentas&id_cuenta="' . $rows->id . '">aquí</a></div>';
+				}
+			}
 		} else {
-			print '<div class="mensaje">La cuenta se ha registrado exitosamente <a href="index.php">Regresar</a></div>';
+			$insertar_cuenta = new connection();
+			$insertar_cuenta->setQuery("insert into tb_cuentas values(default, $id_banco, $tipo_cuenta, '" . $cuenta . "', default)");
+			if(!$insertar_cuenta->getQuery()) {
+				print '<div class="mensaje">Se ha generado un error <a href="index.php">Regresar</a></div>' . pg_last_error();
+			} else {
+				print '<div class="mensaje">La cuenta se ha registrado exitosamente <a href="index.php">Regresar</a></div>';
+			}
 		}
 	}
 	?>
