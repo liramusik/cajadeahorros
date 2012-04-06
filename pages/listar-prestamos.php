2b603243f90232dc7952677211675295f3360ded
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-06 16:17:49 -0430

    Listado

--- pages/listar-prestamos.php
+++ pages/listar-prestamos.php
@@ -11,7 +11,7 @@
 	<script>
 		$(document).ready(function() {
 			$('#listado').dataTable({
-                "bJQueryUI": true, 
+				"bJQueryUI": true, 
 				"bLengthChange": false,
 				"bInfo": false,
 				"aaSorting": [[ 0, "desc" ]],
@@ -35,7 +35,6 @@
 				<th>Monto</th>
 				<th>Fecha</th>
 				<th>Estatus</th>
-				<th>Acciones</th>
 			</tr>
 		</thead>
 		<?php while($rows = pg_fetch_object($c->getQuery())): ?>
@@ -45,23 +44,6 @@
 				<td><div><?php printf("%.2f", $rows->monto); ?></div></td>
 				<td><div><?php print $rows->fecha; ?></div></td>
 				<td><div><?php print $rows->estatus; ?></div></td>
-				<?php if($rows->estatus == 'Pendiente'): ?>
-					<td><div><a href="index.php?page=aprobar-prestamo&prestamo=<?php print $rows->id; ?>"><img src="img/aprobar-prestamo.png" /></a></div></td>
-					<td><div><a href="index.php?page=rechazar-prestamo&prestamo=<?php print $rows->id; ?>"><img src="img/rechazar-prestamo.png" /></a></div></td>
-					<td><div><a href="index.php?page=editar-prestamo&prestamo=<?php print $rows->id; ?>"><img src="img/modificar-prestamo.png" /></a></div></td>
-				<?php elseif($rows->estatus == 'Aprobado'): ?>
-					<td><div style="opacity: .5;"><img src="img/aprobar-prestamo.png" /></div>
-					<div style="opacity: .5;"><img src="img/rechazar-prestamo.png" /></div>
-					<div><a href="index.php?page=editar-prestamo&prestamo=<?php print $rows->id; ?>"><img src="img/modificar-prestamo.png" /></a></div></td>
-				<?php elseif($rows->estatus == 'Rechazado'): ?>
-					<td><div style="opacity: .5;"><img src="img/aprobar-prestamo.png" /></div>
-					<div style="opacity: .5;"><img src="img/rechazar-prestamo.png" /></div>
-					<div style="opacity: .5;"><img src="img/modificar-prestamo.png" /></div></td>
-				<?php elseif($rows->estatus == 'Pagado'): ?>
-					<td><div style="opacity: .5;"><img src="img/aprobar-prestamo.png" /></div>
-					<div style="opacity: .5;"><img src="img/rechazar-prestamo.png" /></div>
-					<div style="opacity: .5;"><img src="img/modificar-prestamo.png" /></div></td>
-				<?php endif; ?>
 			</tr>
 		<?php endwhile; ?>
 	</table>
