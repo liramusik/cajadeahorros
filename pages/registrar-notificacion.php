47479410a0bfcce520946451062154d866824e2c
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-06 17:11:44 -0430

    Registra las notificaciones

---
+++ pages/registrar-notificacion.php
@@ -0,0 +1,23 @@
+<?php
+$emails = preg_split("/\,/", $_POST['emails']);
+$cedulas = preg_split("/\,/", $_POST['cedulas']);
+$fecha = date("Y-m-d H:i:s", time());
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+
+include("../conf/conexion.php");
+$notificar = new conexion();
+
+for($i = 0; $i < sizeof($cedulas) - 1; $i++) {
+	$cedula = $cedulas[$i];
+	$notificar->setQuery("insert into tb_notificaciones values(default, $cedula, '" . $fecha . "', '" . $asunto . "','" . $mensaje . "')");
+	if(!$notificar) {
+		print "Ha ocurrido un error " . pg_last_error();
+	} else {
+		print "El mensaje ha sido enviado";
+	}
+	if(!empty($emails[$i])) {
+		mail($emails[$i], $asunto, wordwrap($mensaje));
+	}
+}
+?>
