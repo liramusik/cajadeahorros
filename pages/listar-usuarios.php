b82e1c7e4b00e016976aac92de36648dc297a9eb
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-06 17:21:55 -0430

    Arreglando el formulario de modificar

--- pages/listar-usuarios.php
+++ pages/listar-usuarios.php
@@ -41,7 +41,7 @@
 				<td><div><?php print $rows->email; ?></div></td>
 				<td>
 					<div class="accion"><a href="#" id="<?php print $rows->cedula; ?>" class="notificar"><img src="img/notificar.png" title="Notificar" alt="Notificar"></a></div>
-					<div class="accion"><a href="<?php print "index.php?page=modificar-usuarios&cedula=" . $rows->cedula; ?>"><img src="img/modificar.png" title="Modificar" alt="Modificar"></a></div>
+					<div class="accion"><a href="#" id="<?php print $rows->cedula; ?>" class="modificar"><img src="img/modificar.png" title="Modificar" alt="Modificar"></a></div>
 					<?php if($_SESSION['session_cedula'] != $rows->cedula): ?>
 						<div class="accion"><a href="<?php print "index.php?page=deshabilitar-usuarios&cedula=" . $rows->cedula; ?>"><img src="img/deshabilitar.png" title="Deshabilitar" alt="Deshabilitar"></a></div>
 					<?php else: ?>
@@ -58,6 +58,11 @@
 	$(document).ready(function() {
 		$('.notificar').click(function() {
 			$('#contenido').load("includes/pages.php?page=notificar&cedula="+$(this).attr('id'));
+		})
+	});
+	$(document).ready(function() {
+		$('.modificar').click(function() {
+			$('#contenido').load("includes/pages.php?page=modificar-usuario&cedula="+$(this).attr('id'));
 		})
 	});
 </script>
