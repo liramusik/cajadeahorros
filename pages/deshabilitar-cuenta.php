6ee667057a526fddc563ae0770ffdfe3dc0d50b1
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-06 20:09:19 -0430

    Añadiendo la desactivación de la cuenta

--- pages/deshabilitar-cuenta.php
+++ pages/deshabilitar-cuenta.php
@@ -1,14 +1,13 @@
-<?php if(isset($_SESSION['session_usuario']) and ($_SESSION['session_id_rol'] == 1)): ?>
-	<?php
-	$cuenta = $_GET['cuenta'];
-	if(isset($cuenta)) {
-		$x = new connection();
-		$x->setQuery("update tb_cuentas set estatus='f' where id=$cuenta;");
-		if($x->getQuery()) {
-			print '<div class="mensaje">La cuenta ha sido deshabilitado con éxito <a href="index.php">Aceptar</a></div>';
-		}
+<?php
+$id = $_POST['id'];
+if(isset($id)) {
+	include("../conf/conexion.php");
+	$deshabilitar_cuenta = new conexion();
+	$deshabilitar_cuenta->setQuery("update tb_cuentas set estatus='f' where id=$id;");
+	if(!$deshabilitar_cuenta->getQuery()) {
+		print 'error ' . pg_last_error();
+	} else {
+		print 'La cuenta ha sido deshabilitada';
 	}
-	?>
-<?php else: ?>
-	<div class="mensaje">Usted no posee privilegios <a href="index.php">Regresar</a></div>
-<?php endif; ?>
+}
+?>
