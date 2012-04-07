88660d3f50447b09bf11e10870a4769e13882583
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-07 00:52:29 -0430

    Añadiendo la fecha y hora del registro

--- pages/deshabilitar-usuario.php
+++ pages/deshabilitar-usuario.php
@@ -1,9 +1,10 @@
 <?php
 $cedula = $_POST['cedula'];
+$fecha = date("yy-m-d H:i:s", time());
 if(isset($cedula)) {
 	include("../conf/conexion.php");
 	$deshabilitar_usuario = new conexion();
-	$deshabilitar_usuario->setQuery("update tb_usuarios set estatus='f' where cedula=$cedula;");
+	$deshabilitar_usuario->setQuery("update tb_usuarios set estatus='f', fecha_egreso='" . $fecha . "' where cedula=$cedula;");
 	if(!$deshabilitar_usuario->getQuery()) {
 		print "Error " . pg_last_error();
 	} else {
