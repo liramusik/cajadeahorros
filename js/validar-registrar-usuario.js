b0976797dda073b730e16352837c76b90c89ef16
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-07 14:21:17 -0430

    Cambiando la validación

---
+++ js/validar-registrar-usuario.js
@@ -0,0 +1,104 @@
+$(document).ready(function() {
+	$("#crear-usuario").validate({
+		rules: {
+			nombre: {
+				required: true,
+				minlength: 3,
+			},
+			apellido: {
+				required: true,
+				minlength: 3,
+			},
+			cedula: {
+				required: true,
+				minlength: 7,
+				digits: true,
+			},
+			telefono: {
+				required: true,
+				minlength: 11,
+				digits: true,
+			},
+			email: {
+				required: true,
+				minlength: 15,
+				email: true,
+			},
+			direccion: {
+				required: true,
+			},
+			fecha_ingreso: {
+				required: true,
+				minlength: 10,
+			},
+			aporte_mensual: { 
+				required: true,
+				minlength: 2,
+				digits: true,
+			},
+			usuario: {
+				required: true,
+				minlength: 5,
+			},
+			password: {
+				required: true,
+				minlength: 5,
+			},
+			confirmar_password: {
+				required: true,
+				minlength: 5,
+				equalTo: "#password",
+			},
+		},
+	});
+});
+$(document).ready(function() {
+	$("#cedula").keydown(function(event) {
+		if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39)) {
+			return;
+		} else {
+			if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+				event.preventDefault(); 
+			}
+		}
+	});
+});
+$(document).ready(function() {
+	$("#telefono").keydown(function(event) {
+		if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39)) {
+			return;
+		} else {
+			if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+				event.preventDefault(); 
+			}
+		}
+	});
+});
+$(document).ready(function() {
+	$("#aporte_mensual").keydown(function(event) {
+		if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39)) {
+			return;
+		} else {
+			if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+				event.preventDefault(); 
+			}
+		}
+	});
+});
+$(document).ready(function() {
+	$("#fecha_ingreso").datepicker({
+		dateFormat: 'dd/mm/yy',
+		dayNamesMin: ['Dom', 'Lun', 'Mar', 'Mier', 'Jue', 'Vier', 'Sab'],
+		monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Séptiembre', 'Octubre', 'Noviembre', 'Diciembre']
+	});
+});
+$(document).ready(function() { 
+	var opciones = {
+		success: mostrarRespuesta,
+	};
+	$('.form').ajaxForm(opciones);
+	function mostrarRespuesta(responseText) {
+		alert("Mensaje: " + responseText);
+		$('.form').resetForm();
+	}; 
+}); 
--- pages/usuarios.php
+++ pages/usuarios.php
@@ -6,118 +6,6 @@
 	$y->setQuery("select * from tb_nacionalidad;");
 	?>
 
-	<script type="text/javascript">
-		$(document).ready(function(){
-			$("#crear-usuario").validate({
-				rules: {
-					nombre: {
-						required: true,
-						minlength: 3,
-					},
-					apellido: {
-						required: true,
-						minlength: 3,
-					},
-					cedula: {
-						required: true,
-						minlength: 7,
-						digits: true
-					},
-					telefono: {
-						required: true,
-						minlength: 11,
-						digits: true
-					},
-					email: {
-						required: true,
-						minlength: 15,
-						email: true
-					},
-					direccion: {
-						required: true,
-					},
-					fecha_ingreso: {
-						required: true,
-						minlength: 10,
-                    },
-                    aporte_mensual: { 
-                        required: true,
-                        minlength: 2,
-                        digits: true,
-                        },
-                    usuario: {
-						required: true,
-						minlength: 5,
-					},
-					password: {
-						required: true,
-						minlength: 5
-					},
-					confirmar_password: {
-						required: true,
-						minlength: 5,
-						equalTo: "#password"
-					}
-				},
-			});
-		});
-		$(document).ready(function() {
-			$("#cedula").keydown(function(event) {
-                    if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39)) {
-                         return;
-                    } else {
-                        if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
-                            event.preventDefault(); 
-                        }   
-                    }
-                });
-            });
-            $(document).ready(function() {
-                $("#telefono").keydown(function(event) {
-                    if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39)) {
-                         return;
-                    } else {
-                        if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
-                            event.preventDefault(); 
-                        }   
-                    }
-                });
-            });
-		$(document).ready(function() {
-			$("#aporte_mensual").keydown(function(event) {
-                    if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39)) {
-                         return;
-                    } else {
-                        if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
-                            event.preventDefault(); 
-                        }   
-                    }
-                });
-            });
-		$(function() {
-			$("#fecha_ingreso").datepicker({
-                dateFormat: 'dd/mm/yy',
-                    dayNamesMin: ['Dom', 'Lun', 'Mar', 'Mier', 'Jue', 'Vier', 'Sab'],
-                    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Séptiembre', 'Octubre', 'Noviembre', 'Diciembre']
-			});
-			$("#fecha_egreso").datepicker({
-                dateFormat: 'dd/mm/yy',
-                dayNamesMin: ['Dom', 'Lun', 'Mar', 'Mier', 'Jue', 'Vier', 'Sab'],
-                monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Séptiembre', 'Octubre', 'Noviembre', 'Diciembre']
-			});
-		});
-		$(document).ready(function() { 
-			var opciones = {
-				success: mostrarRespuesta,
-			};
-			$('.form').ajaxForm(opciones);
-			function mostrarRespuesta(responseText) {
-				alert("Mensaje: " + responseText);
-				$('.form').resetForm();
-			}; 
-		}); 
-	</script>
-
 	<h1>Crear Usuario</h1>
 	<form action="pages/registrar-usuario.php" method="post" id="crear-usuario" class="form">
 		<fieldset>
@@ -239,4 +127,6 @@
 		</fieldset>
 		<input id="submit" type="submit" value="Registrar" name="submit" class="boton1"/>
 	</form>
+	<script type="text/javascript" src="/cajadeahorros/js/validar-registrar-usuario.js">
+	</script>
 <?php endif; ?>
