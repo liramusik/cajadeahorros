f4171c2dd2e79a0c1ef39f9e3cbc5fed1d053a6e
liramusik <liramusik@gmail.com>
2012-04-07 01:05:13 -0430

    Realizando cambios

--- pages/usuarios.php
+++ pages/usuarios.php
@@ -59,26 +59,26 @@
 		});
 		$(document).ready(function() {
 			$("#cedula").keydown(function(event) {
-				if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39)) {
-					 return;
-				} else {
-					if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
-						event.preventDefault(); 
-					}   
-				}
-			});
-		});
-		$(document).ready(function() {
-			$("#telefono").keydown(function(event) {
-				if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39)) {
-					 return;
-				} else {
-					if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
-						event.preventDefault(); 
-					}   
-				}
-			});
-		});
+                    if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39)) {
+                         return;
+                    } else {
+                        if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                            event.preventDefault(); 
+                        }   
+                    }
+                });
+            });
+            $(document).ready(function() {
+                $("#telefono").keydown(function(event) {
+                    if(event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39)) {
+                         return;
+                    } else {
+                        if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                            event.preventDefault(); 
+                        }   
+                    }
+                });
+            });
 		$(function() {
 			$("#fecha_ingreso").datepicker({
                 dateFormat: 'dd/mm/yy',
