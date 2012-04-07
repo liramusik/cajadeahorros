88d26fa3e1828a9fb33658dff107701eeafd9b4c
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-07 14:36:19 -0430

    Agregando restricciones

--- pages/listar-transacciones.php
+++ pages/listar-transacciones.php
@@ -14,9 +14,9 @@
 	<?php
 	$cedula = $_GET['cedula'];
 	$c = new conexion();
-	if ($_SESSION['session_id_rol']==1) {
+	if($_SESSION['session_id_rol'] == 1) {
 		$c->setQuery("select t.id, u.nombre, u.apellido, c.cuenta, b.nombre as banco, to_char(t.fecha,'DD/MM/YYYY') as fecha, tt.tipo, t.monto, t.deposito, e.estatus from tb_transacciones as t inner join (tb_cuentas as c left join tb_bancos as b on  c.id_banco = b.id) on t.id_cuenta = c.id left join tb_estatus_transacciones as e on  e.id = t.id_estatus_transaccion left join tb_usuarios as u on u.cedula = t.cedula_usuario left join tb_tipo_transacciones as tt on tt.id = t.id_tipo_transaccion;");
-	} elseif($_SESSION['session_id_rol']==2) {
+	} elseif($_SESSION['session_id_rol'] == 2) {
 		$c->setQuery("select t.id, u.nombre, u.apellido, c.cuenta, b.nombre as banco, to_char(t.fecha,'DD/MM/YYYY') as fecha, tt.tipo, t.monto, t.deposito, e.estatus from tb_transacciones as t inner join (tb_cuentas as c left join tb_bancos as b on  c.id_banco = b.id) on t.id_cuenta = c.id left join tb_estatus_transacciones as e on  e.id = t.id_estatus_transaccion left join tb_usuarios as u on u.cedula = t.cedula_usuario left join tb_tipo_transacciones as tt on tt.id = t.id_tipo_transaccion where cedula=$cedula;");
 	}
 	?>
@@ -50,7 +50,9 @@
 				<th>Monto</th>
 				<th>Deposito</th>
 				<th>Estatus</th>
-				<th>Acciones</th>
+				<?php if($_SESSION['session_id_rol'] == 1): ?>
+					<th>Acciones</th>
+				<?php endif; ?>
 			</tr>
 		</thead>
 		<?php while($rows = pg_fetch_object($c->getQuery())): ?>
@@ -63,20 +65,22 @@
 				<td><div><?php print $rows->monto; ?></div></td>
 				<td><div><?php print $rows->deposito; ?></div></td>
 				<td><div><?php print $rows->estatus; ?></div></td>
-				<td>
-					<div class="accion">
-						<form action="pages/aceptar-transaccion.php" method="post" id="aceptar-transaccion" class="form">
-							<input type="hidden" name="id" value="<?php print $rows->id; ?>" />
-							<input type="submit" name="submit" id="boton-aceptar" />
-						</form>
-					</div>
-					<div class="accion">
-						<form action="pages/rechazar-transaccion.php" method="post" id="rechazar-transaccion" class="form">
-							<input type="hidden" name="id" value="<?php print $rows->id; ?>" />
-							<input type="submit" name="submit" id="boton-rechazar" />
-						</form>
-					</div>
-				</td>
+				<?php if($_SESSION['session_id_rol']==1): ?>
+					<td>
+						<div class="accion">
+							<form action="pages/aceptar-transaccion.php" method="post" id="aceptar-transaccion" class="form">
+								<input type="hidden" name="id" value="<?php print $rows->id; ?>" />
+								<input type="submit" name="submit" id="boton-aceptar" />
+							</form>
+						</div>
+						<div class="accion">
+							<form action="pages/rechazar-transaccion.php" method="post" id="rechazar-transaccion" class="form">
+								<input type="hidden" name="id" value="<?php print $rows->id; ?>" />
+								<input type="submit" name="submit" id="boton-rechazar" />
+							</form>
+						</div>
+					</td>
+				<?php endif; ?>
 			</tr>
 		<?php endwhile; ?>
 	</table>
