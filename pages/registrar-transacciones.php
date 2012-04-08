7f7ceacd67aae5abaf7bcc2368c1ed7e2f6914ca
David Mora <gochirila000@yahoo.es>
2012-04-07 22:14:17 -0430

    registrar transaccion execente

--- pages/registrar-transacciones.php
+++ pages/registrar-transacciones.php
@@ -12,6 +12,21 @@ $deposito = $_POST['deposito'];
 $fecha_split = preg_split("/\//", $fechas);
 $fecha = $fecha_split[2] . '-' . $fecha_split[1] . '-' . $fecha_split[0];
 
+if($tipo == 1) {
+	$buscar = new conexion();
+	$buscar->setQuery("select aporte_mensual from tb_usuarios where cedula=$cedula");
+	while($rows = pg_fetch_object($buscar->getQuery())) {
+	 	$aporte = $rows->aporte_mensual;
+	}
+			
+	if($aporte < $monto){
+		$exedente = $monto - $aporte;
+		$monto = $monto - $exedente;
+		$insertar_exedente = new conexion();
+		$insertar_exedente->setQuery("insert into tb_transacciones values(default, $cedula, $cuenta, 5, default, '" . $fecha . "', $exedente, $deposito);");
+	} 
+}
+
 $insertar_transacciones = new conexion();
 $insertar_transacciones->setQuery("insert into tb_transacciones values(default, $cedula, $cuenta, $tipo, default, '" . $fecha . "' , $monto, $deposito);");
 
