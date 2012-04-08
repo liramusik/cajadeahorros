68ed8f96b99444325bde364fedc044b4e2f9b4b3
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-08 01:56:09 -0430

    Arreglando préstamos

---
+++ pages/ver-prestamo-transacciones-usuario.php
@@ -0,0 +1,42 @@
+<h1>Detalle de Préstamo</h1>
+
+<div class="detalle-prestamo">
+	<table>
+		<tr>
+			<td><div class="etiqueta">Estatus:</div></td>
+			<td><div><?php print $estatus; ?></div></td>
+		</tr>
+		<tr>
+			<td><div class="etiqueta">Nombre:</div></td>
+			<td><div><?php print $nombre. " " . $apellido; ?></div></td>
+		</tr>
+		<tr>
+			<td><div class="etiqueta">Cédula:</div></td>
+			<td><div><?php print $cedula; ?></div></td>
+		</tr>
+		<tr>
+			<td><div class="etiqueta">Monto:</div></td>
+			<td><div><?php print $monto; ?></div></td>
+		</tr>
+		<tr>
+			<td><div class="etiqueta">Fecha:</div></td>
+			<td><div><?php print $fecha; ?></div></td>
+		</tr>
+		<tr>
+			<td><div class="etiqueta">tiempo:</div></td>
+			<td><div><?php print $tiempo; ?></div></td>
+		</tr>
+		<tr>
+			<td><div class="etiqueta">Porcentaje:</div></td>
+			<td><div><?php print $porcentaje; ?></div></td>
+		</tr>
+		<tr>
+			<td><div class="etiqueta">Observación:</div></td>
+			<td><div><?php print $observacion; ?></div></td>
+		</tr>
+		<tr>
+			<td><div class="etiqueta">Respuesta:</div></td>
+			<td><div><?php print $respuesta; ?></div></td>
+		</tr>
+	</table>
+</div>
