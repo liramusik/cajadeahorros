22b5b426050c27469c5f3ad6dd8e3cd87fcb4e57
liramusik <liramusik@gmail.com>
2012-04-08 16:06:17 -0430

    Arreglando el query para que no salgan las cuentas que no tienen intereses

--- pages/reporte-listar-intereses.php
+++ pages/reporte-listar-intereses.php
@@ -2,8 +2,10 @@
 include("../conf/wkpdf.php");
 if(isset($_SESSION['session_usuario']) && ($_SESSION['session_id_rol'] == 1)) {
 	$c = new conexion();
-	$c->setQuery("select tb_cuentas.id, nombre, tipo, cuenta, monto, to_char(fecha_interes,'DD/MM/YYYY') as fecha from tb_cuentas left join tb_bancos on tb_cuentas.id_banco = tb_bancos.id left join tb_tipo_cuentas on tb_cuentas.id_tipo_cuenta = tb_tipo_cuentas.id left join tb_intereses on tb_cuentas.id = tb_intereses.id_cuenta;");
-	$pdf = 
+	$c->setQuery("select nombre, tipo, cuenta, monto, to_char(fecha_interes, 'DD/MM/YYYY') as fecha  from tb_intereses left join tb_cuentas on tb_intereses.id_cuenta = tb_cuentas.id left join tb_bancos on tb_cuentas.id_banco = tb_bancos.id left join tb_tipo_cuentas on tb_tipo_cuentas.id = tb_cuentas.id_tipo_cuenta;");
+	$suma = new conexion();
+        $suma->setQuery("select sum(monto) as suma from tb_cuentas left join tb_bancos on tb_cuentas.id_banco = tb_bancos.id left join tb_tipo_cuentas on tb_cuentas.id_tipo_cuenta = tb_tipo_cuentas.id left join tb_intereses on tb_cuentas.id = tb_intereses.id_cuenta;");
+        $pdf = 
 		"<style type='text/css'>"
 		."table { 
 			border: 1px solid green;
