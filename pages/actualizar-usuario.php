382f6d32cc4e0ab63b565dfe1e8d8ea365a6cb7a
Héctor Alonso Lozada Echezuría <imatsu@gmail.com>
2012-04-11 02:12:29 -0430

    Posponiendo reparación

--- pages/actualizar-usuario.php
+++ pages/actualizar-usuario.php
@@ -9,20 +9,19 @@ $post_apellido = $_POST['apellido'];
 $post_telefono = $_POST['telefono'];
 $post_email = $_POST['email'];
 $post_direccion = $_POST['direccion'];
-$aporte_mensual = $_POST['aporte_mensual'];
 $post_password = $_POST['password'];
 
 $actualizar_usuarios=new conexion();
 
 if(empty($post_password)) {
-	$actualizar_usuarios->setQuery("update tb_usuarios set nombre='" . $post_nombre . "', apellido='" . $post_apellido . "', telefono='" . $post_telefono . "',  email = '" . $post_email . "', direccion = '" . $post_direccion . "', aporte_mensual=$aporte_mensual where cedula=$cedula");
+	$actualizar_usuarios->setQuery("update tb_usuarios set nombre='" . $post_nombre . "', apellido='" . $post_apellido . "', telefono='" . $post_telefono . "',  email = '" . $post_email . "', direccion = '" . wordwrap($post_direccion) . "' where cedula=$cedula");
 	if(!$actualizar_usuarios->getQuery()){
 		print "No se pudo modificar el usuario";
 	} else {
 		print "El usuario se actualizo con exito";
 	}	
 } else {
-	$actualizar_usuarios->setQuery("update tb_usuarios set nombre='" . $post_nombre . "', apellido='" . $post_apellido . "', telefono='" . $post_telefono . "',  email = '" . $post_email . "', direccion = '" . $post_direccion . "', aporte_mensual=$aporte_mensual, password='" . md5($post_password) . "' where cedula=$cedula");
+	$actualizar_usuarios->setQuery("update tb_usuarios set nombre='" . $post_nombre . "', apellido='" . $post_apellido . "', telefono='" . $post_telefono . "',  email = '" . $post_email . "', direccion = '" . $post_direccion . "', password='" . md5($post_password) . "' where cedula=$cedula");
 	if(!$actualizar_usuarios->getQuery()) {
 		print "No se pudo modificar el  usuario";
 	} else {
