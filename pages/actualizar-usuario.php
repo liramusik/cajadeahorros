7fb486fb9030181dcd96cb6f7fc538c6e8ba0fd1
liramusik <liramusik@gmail.com>
2012-04-11 02:30:35 -0430

    Acentuando la palabra éxito

--- pages/actualizar-usuario.php
+++ pages/actualizar-usuario.php
@@ -18,14 +18,14 @@ if(empty($post_password)) {
 	if(!$actualizar_usuarios->getQuery()){
 		print "No se pudo modificar el usuario";
 	} else {
-		print "El usuario se actualizo con exito";
+		print "El usuario se actualizo con éxito";
 	}	
 } else {
 	$actualizar_usuarios->setQuery("update tb_usuarios set nombre='" . $post_nombre . "', apellido='" . $post_apellido . "', telefono='" . $post_telefono . "',  email = '" . $post_email . "', direccion = '" . $post_direccion . "', password='" . md5($post_password) . "' where cedula=$cedula");
 	if(!$actualizar_usuarios->getQuery()) {
 		print "No se pudo modificar el  usuario";
 	} else {
-		print "El usuario se actualizo con exito";
+		print "El usuario se actualizo con éxito";
 	}
 }
 ?>
